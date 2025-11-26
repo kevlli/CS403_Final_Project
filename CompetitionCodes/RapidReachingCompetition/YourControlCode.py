787e895eca30e3bbb480288fe781c8aec07a3f32
Kevin Li <kevinlouisli7@gmail.com>
2025-11-25 19:27:05 -0500

    Edited code which works on 6/8 points, needs fruther debugging

--- CompetitionCodes/RapidReachingCompetition/YourControlCode.py
+++ CompetitionCodes/RapidReachingCompetition/YourControlCode.py
@@ -17,30 +17,38 @@ class YourCtrl:
     self.ee_id = mujoco.mj_name2id(self.m, mujoco.mjtObj.mjOBJ_BODY, "EE_Frame")
     self.current_idx = 0
 
-  def getIK(self, goal, initq):
-    #TODO: add orientation error caculation, keep current dataT.xquat[self.ee_id].copy()
-    dataT = mujoco.MjData(self.m)
+  def getIK(self, target_position):
+    #TODO: add orientation error caculation, keep current self.d.xquat[self.ee_id].copy()
     intr = 0
-    dataT.qpos[:] = initq.copy() 
-    mujoco.mj_forward(self.m, dataT)    
 
-    dx = goal-dataT.xpos[self.ee_id].copy()
+    point = np.zeros(3)
+    ee_id = self.ee_id
+    dx = target_position-self.d.xpos[ee_id].copy()
+    jacp =  np.zeros((3, self.m.nv)) 
+    jcar = np.zeros((3, self.m.nv))
 
-    while np.linalg.norm(dx) > 0.01 and intr<100:
-      jacp =  np.zeros((3, self.m.nv)) 
-      jcar = np.zeros((3, self.m.nv))
-      mujoco.mj_jac(self.m, dataT, jacp, jcar, dataT.xpos[self.ee_id], self.ee_id)
+    initial_jpos = np.copy(self.d.qpos[:6])
+    target_jpos = np.copy(initial_jpos)
+
+    while np.linalg.norm(dx) >= 0.01 and intr<3:
+      mujoco.mj_jac(self.m, self.d, jacp, jcar, self.d.xpos[self.ee_id], self.ee_id)
+      EE_pos = self.d.body(ee_id).xpos
       J = jacp.copy()
+      dx = target_position-EE_pos
       
       dq = J.T @ np.linalg.inv(J@J.T+0.01**2*np.eye(3))@dx
 
-      dataT.qpos[:] = dataT.qpos[:]+0.01*dq
+      target_jpos += 0.1*dq
 
-      mujoco.mj_forward(self.m, dataT)
+      self.d.qpos[:6] = target_jpos 
+      mujoco.mj_kinematics(self.m, self.d)
       
-      dx = goal-dataT.xpos[self.ee_id].copy()
+      
       intr+=1
-    return dataT.qpos[:]
+    self.d.qpos[:6] = np.copy(initial_jpos)
+    jpos_error = target_jpos - self.d.qpos[:6]
+    velocity = self.d.qvel[:6]
+    return jpos_error, velocity
   
   def best_path(self, target_points):
     #determine either the best path or the next best node to get
@@ -49,23 +57,23 @@ class YourCtrl:
   def CtrlUpdate(self):
     
     #call best_path
-    goal = self.target_points[:, self.current_idx]
+    target_position = self.target_points[:, self.current_idx]
 
     ee_pos = self.d.xpos[self.ee_id].copy()
-    distance = np.linalg.norm(ee_pos - goal)
+    distance = np.linalg.norm(ee_pos - target_position)
     if distance < 0.01:
       #call bestpath?
       self.current_idx+=1
-      goal = self.target_points[:, self.current_idx]
+      target_position = self.target_points[:, self.current_idx]
       if self.current_idx == 8:
         return np.zeros((6,6))
 
-    q_d = self.getIK(goal, self.d.qpos)
+    jpos_error, velocity = self.getIK(target_position)
 
     M = np.zeros((6,6))
     mujoco.mj_fullM(self.m, M, self.d.qM)  
 
     #TODO:Use Operational Space Dynamics from HW6 and lecturue 22
-    jtorque_cmd = M@(self.kp*(q_d - self.d.qpos) - self.kd*self.d.qvel)+ self.d.qfrc_bias
+    jtorque_cmd = M@(self.kp*(jpos_error) - self.kd*velocity)+ self.d.qfrc_bias
 
     return jtorque_cmd
